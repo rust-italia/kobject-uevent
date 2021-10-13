7320b65e5cf173e5078b303b9b67fbccece687af
Edoardo Morandi <morandidodo@gmail.com>
2021-10-13 20:33:01 +0200

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ impl FromStr for ActionType {
 }
 
 /// Linux kernel userspace event
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct UEvent {
     /// Action happening
     pub action: ActionType,
@@ -85,5 +85,332 @@ impl UEvent {
             env,
             seq,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! uevent {
+        (
+            action: $action:expr,
+             devpath: $devpath:expr,
+             subsystem: $subsystem:expr,
+             env: { $($env_name:expr => $env_value:expr),* $(,)? },
+             seq: $seq:expr
+         ) => {
+            UEvent {
+                action: $action,
+                devpath: PathBuf::from($devpath),
+                subsystem: $subsystem.to_string(),
+                env: IntoIterator::into_iter([
+                    $(($env_name.to_string(), $env_value.to_string())),*
+                ]).collect(),
+                seq: $seq,
+            }
+        };
+    }
+
+    #[test]
+    fn add_uevent() {
+        const DATA: &[u8] = b"add@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=add\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SYNTH_UUID=0\0\
+                              MAJOR=4\0\
+                              MINOR=70\0\
+                              DEVNAME=ttyS6\0\
+                              SEQNUM=3469";
+        assert_eq!(
+            UEvent::from_netlink_packet(DATA).unwrap(),
+            uevent! {
+                action: ActionType::Add,
+                devpath: "/devices/platform/serial8250/tty/ttyS6",
+                subsystem: "tty",
+                env: {
+                    "ACTION" => "add",
+                    "DEVPATH" => "/devices/platform/serial8250/tty/ttyS6",
+                    "SUBSYSTEM" => "tty",
+                    "SYNTH_UUID" => "0",
+                    "MAJOR" => "4",
+                    "MINOR" => "70",
+                    "DEVNAME" => "ttyS6",
+                    "SEQNUM" => "3469",
+                },
+                seq: 3469
+            }
+        );
+    }
+
+    #[test]
+    fn remove_uevent() {
+        const DATA: &[u8] = b"remove@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=remove\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SYNTH_UUID=0\0\
+                              MAJOR=4\0\
+                              MINOR=70\0\
+                              DEVNAME=ttyS6\0\
+                              SEQNUM=3471";
+        assert_eq!(
+            UEvent::from_netlink_packet(DATA).unwrap(),
+            uevent! {
+                action: ActionType::Remove,
+                devpath: "/devices/platform/serial8250/tty/ttyS6",
+                subsystem: "tty",
+                env: {
+                    "ACTION" => "remove",
+                    "DEVPATH" => "/devices/platform/serial8250/tty/ttyS6",
+                    "SUBSYSTEM" => "tty",
+                    "SYNTH_UUID" => "0",
+                    "MAJOR" => "4",
+                    "MINOR" => "70",
+                    "DEVNAME" => "ttyS6",
+                    "SEQNUM" => "3471",
+                },
+                seq: 3471
+            }
+        );
+    }
+
+    #[test]
+    fn change_uevent() {
+        const DATA: &[u8] = b"change@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=change\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SYNTH_UUID=0\0\
+                              MAJOR=4\0\
+                              MINOR=70\0\
+                              DEVNAME=ttyS6\0\
+                              SEQNUM=3472";
+        assert_eq!(
+            UEvent::from_netlink_packet(DATA).unwrap(),
+            uevent! {
+                action: ActionType::Change,
+                devpath: "/devices/platform/serial8250/tty/ttyS6",
+                subsystem: "tty",
+                env: {
+                    "ACTION" => "change",
+                    "DEVPATH" => "/devices/platform/serial8250/tty/ttyS6",
+                    "SUBSYSTEM" => "tty",
+                    "SYNTH_UUID" => "0",
+                    "MAJOR" => "4",
+                    "MINOR" => "70",
+                    "DEVNAME" => "ttyS6",
+                    "SEQNUM" => "3472",
+                },
+                seq: 3472
+            }
+        );
+    }
+
+    #[test]
+    fn move_uevent() {
+        const DATA: &[u8] = b"move@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=move\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SYNTH_UUID=0\0\
+                              MAJOR=4\0\
+                              MINOR=70\0\
+                              DEVNAME=ttyS6\0\
+                              SEQNUM=3473";
+        assert_eq!(
+            UEvent::from_netlink_packet(DATA).unwrap(),
+            uevent! {
+                action: ActionType::Move,
+                devpath: "/devices/platform/serial8250/tty/ttyS6",
+                subsystem: "tty",
+                env: {
+                    "ACTION" => "move",
+                    "DEVPATH" => "/devices/platform/serial8250/tty/ttyS6",
+                    "SUBSYSTEM" => "tty",
+                    "SYNTH_UUID" => "0",
+                    "MAJOR" => "4",
+                    "MINOR" => "70",
+                    "DEVNAME" => "ttyS6",
+                    "SEQNUM" => "3473",
+                },
+                seq: 3473
+            }
+        );
+    }
+
+    #[test]
+    fn online_uevent() {
+        const DATA: &[u8] = b"online@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=online\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SYNTH_UUID=0\0\
+                              MAJOR=4\0\
+                              MINOR=70\0\
+                              DEVNAME=ttyS6\0\
+                              SEQNUM=3474";
+        assert_eq!(
+            UEvent::from_netlink_packet(DATA).unwrap(),
+            uevent! {
+                action: ActionType::Online,
+                devpath: "/devices/platform/serial8250/tty/ttyS6",
+                subsystem: "tty",
+                env: {
+                    "ACTION" => "online",
+                    "DEVPATH" => "/devices/platform/serial8250/tty/ttyS6",
+                    "SUBSYSTEM" => "tty",
+                    "SYNTH_UUID" => "0",
+                    "MAJOR" => "4",
+                    "MINOR" => "70",
+                    "DEVNAME" => "ttyS6",
+                    "SEQNUM" => "3474",
+                },
+                seq: 3474
+            }
+        );
+    }
+
+    #[test]
+    fn offline_uevent() {
+        const DATA: &[u8] = b"offline@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=offline\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SYNTH_UUID=0\0\
+                              MAJOR=4\0\
+                              MINOR=70\0\
+                              DEVNAME=ttyS6\0\
+                              SEQNUM=3475";
+        assert_eq!(
+            UEvent::from_netlink_packet(DATA).unwrap(),
+            uevent! {
+                action: ActionType::Offline,
+                devpath: "/devices/platform/serial8250/tty/ttyS6",
+                subsystem: "tty",
+                env: {
+                    "ACTION" => "offline",
+                    "DEVPATH" => "/devices/platform/serial8250/tty/ttyS6",
+                    "SUBSYSTEM" => "tty",
+                    "SYNTH_UUID" => "0",
+                    "MAJOR" => "4",
+                    "MINOR" => "70",
+                    "DEVNAME" => "ttyS6",
+                    "SEQNUM" => "3475",
+                },
+                seq: 3475
+            }
+        );
+    }
+
+    #[test]
+    fn bind_uevent() {
+        const DATA: &[u8] = b"bind@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=bind\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SYNTH_UUID=0\0\
+                              MAJOR=4\0\
+                              MINOR=70\0\
+                              DEVNAME=ttyS6\0\
+                              SEQNUM=3476";
+        assert_eq!(
+            UEvent::from_netlink_packet(DATA).unwrap(),
+            uevent! {
+                action: ActionType::Bind,
+                devpath: "/devices/platform/serial8250/tty/ttyS6",
+                subsystem: "tty",
+                env: {
+                    "ACTION" => "bind",
+                    "DEVPATH" => "/devices/platform/serial8250/tty/ttyS6",
+                    "SUBSYSTEM" => "tty",
+                    "SYNTH_UUID" => "0",
+                    "MAJOR" => "4",
+                    "MINOR" => "70",
+                    "DEVNAME" => "ttyS6",
+                    "SEQNUM" => "3476",
+                },
+                seq: 3476
+            }
+        );
+    }
+
+    #[test]
+    fn unbind_uevent() {
+        const DATA: &[u8] = b"unbind@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=unbind\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SYNTH_UUID=0\0\
+                              MAJOR=4\0\
+                              MINOR=70\0\
+                              DEVNAME=ttyS6\0\
+                              SEQNUM=3477";
+        assert_eq!(
+            UEvent::from_netlink_packet(DATA).unwrap(),
+            uevent! {
+                action: ActionType::Unbind,
+                devpath: "/devices/platform/serial8250/tty/ttyS6",
+                subsystem: "tty",
+                env: {
+                    "ACTION" => "unbind",
+                    "DEVPATH" => "/devices/platform/serial8250/tty/ttyS6",
+                    "SUBSYSTEM" => "tty",
+                    "SYNTH_UUID" => "0",
+                    "MAJOR" => "4",
+                    "MINOR" => "70",
+                    "DEVNAME" => "ttyS6",
+                    "SEQNUM" => "3477",
+                },
+                seq: 3477
+            }
+        );
+    }
+
+    #[test]
+    fn invalid_event() {
+        const DATA: &[u8] = b"hello@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=hello\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SEQNUM=3477";
+        assert!(UEvent::from_netlink_packet(DATA).is_err());
+    }
+
+    #[test]
+    fn missing_action() {
+        const DATA: &[u8] = b"add@/devices/platform/serial8250/tty/ttyS6\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty\0\
+                              SEQNUM=3477";
+        assert!(UEvent::from_netlink_packet(DATA).is_err());
+    }
+
+    #[test]
+    fn missing_devpath() {
+        const DATA: &[u8] = b"add@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=unbind\0\
+                              SUBSYSTEM=tty\0\
+                              SEQNUM=3477";
+        assert!(UEvent::from_netlink_packet(DATA).is_err());
+    }
+
+    #[test]
+    fn missing_subsystem() {
+        const DATA: &[u8] = b"add@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=unbind\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SEQNUM=3477";
+        assert!(UEvent::from_netlink_packet(DATA).is_err());
+    }
+
+    #[test]
+    fn missing_seqnum() {
+        const DATA: &[u8] = b"add@/devices/platform/serial8250/tty/ttyS6\0\
+                              ACTION=unbind\0\
+                              DEVPATH=/devices/platform/serial8250/tty/ttyS6\0\
+                              SUBSYSTEM=tty";
+        assert!(UEvent::from_netlink_packet(DATA).is_err());
     }
 }
